667842739e038e1f16963fa9da21d623755c7932
Gelrignard <guenlin@seas.upenn.edu>
2024-11-25 21:24:50 -0500

    saved. I need to rest.

--- transform_to_pkl_2.py
+++ transform_to_pkl_2.py
@@ -1,13 +1,13 @@
+import os
 import numpy as np
 import functools
 import tensorflow as tf
-import os
 import json
 import argparse
 import pickle
 
-# Eager execution is enabled by default in TensorFlow 2.x
-
+# Global debug flag
+DEBUG = False
 
 _FEATURE_DESCRIPTION = {
     'position': tf.io.VarLenFeature(tf.string),
@@ -38,7 +38,12 @@ def _read_metadata(data_path):
         return json.loads(fp.read())
 
 def convert_to_tensor(value, encoded_dtype):
-    return tf.convert_to_tensor(value, dtype=encoded_dtype)
+    if DEBUG:
+        print(f"Converting value to tensor with dtype {encoded_dtype}")
+    tensor = tf.convert_to_tensor(value, dtype=encoded_dtype)
+    if DEBUG:
+        print(f"Converted tensor: {tensor}")
+    return tensor
 
 def parse_serialized_simulation_example(example_proto, metadata):
     if 'context_mean' in metadata:
@@ -50,14 +55,20 @@ def parse_serialized_simulation_example(example_proto, metadata):
         context_features=_CONTEXT_FEATURES,
         sequence_features=feature_description)
     for feature_key, item in parsed_features.items():
+        if DEBUG:
+            print(f"Parsing feature: {feature_key}")
         convert_fn = functools.partial(
             convert_to_tensor, encoded_dtype=_FEATURE_DTYPES[feature_key]['in'])
         parsed_features[feature_key] = tf.py_function(
             convert_fn, inp=[item.values], Tout=_FEATURE_DTYPES[feature_key]['out'])
+        if DEBUG:
+            print(f"Parsed feature {feature_key}: {parsed_features[feature_key]}")
 
     position_shape = [metadata['sequence_length'] + 1, -1, metadata['dim']]
     parsed_features['position'] = tf.reshape(parsed_features['position'],
                                              position_shape)
+    if DEBUG:
+        print(f"Reshaped position: {parsed_features['position']}")
 
     sequence_length = metadata['sequence_length'] + 1
     if 'context_mean' in metadata:
@@ -65,12 +76,16 @@ def parse_serialized_simulation_example(example_proto, metadata):
         parsed_features['step_context'] = tf.reshape(
             parsed_features['step_context'],
             [sequence_length, context_feat_len])
+        if DEBUG:
+            print(f"Reshaped step context: {parsed_features['step_context']}")
 
     context['particle_type'] = tf.py_function(
         functools.partial(convert_to_tensor, encoded_dtype=np.int64),
         inp=[context['particle_type'].values],
         Tout=[tf.int64])
     context['particle_type'] = tf.reshape(context['particle_type'], [-1])
+    if DEBUG:
+        print(f"Reshaped particle type: {context['particle_type']}")
 
     return context, parsed_features
 
@@ -78,8 +93,12 @@ def parse_arguments():
     parser = argparse.ArgumentParser("Transform options.")
     parser.add_argument('--dataset', default='Water', type=str, help='dataset to transform.')
     parser.add_argument('--split', default='train', choices=['train', 'test', 'valid'], help='split to transform.')
+    parser.add_argument('--debug', action='store_true', help='Enable debug mode for detailed output.')
 
     args = parser.parse_args()
+
+    global DEBUG
+    DEBUG = args.debug
 
     return args
 
@@ -98,15 +117,22 @@ def transform(args):
         positions.append(parsed_features['position'].numpy())
         if 'step_context' in parsed_features:
             step_context.append(parsed_features['step_context'].numpy())
+
+    if DEBUG:
+        print(f"Saving positions to datasets/{args.dataset}/{args.split}/positions.pkl")
     position_file = open(f'datasets/{args.dataset}/{args.split}/positions.pkl', 'wb')
     pickle.dump(positions, position_file)
     position_file.close()
 
+    if DEBUG:
+        print(f"Saving particle types to datasets/{args.dataset}/{args.split}/particle_types.pkl")
     particle_file = open(f'datasets/{args.dataset}/{args.split}/particle_types.pkl', 'wb')
     pickle.dump(particle_types, particle_file)
     particle_file.close()
 
     if len(step_context) != 0:
+        if DEBUG:
+            print(f"Saving step context to datasets/{args.dataset}/{args.split}/step_context.pkl")
         context_file = open(f'datasets/{args.dataset}/{args.split}/step_context.pkl', 'wb')
         pickle.dump(step_context, context_file)
         context_file.close()
